85304c22419ad4ac1edc48f60d20bd7ad60e70e7
mucyoalexaxel <mucyoalexaxel@gmail.com>
2022-01-31 17:13:20 +0200

    Mobile Projects Section

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -110,6 +110,20 @@ modalCloses.forEach((modalClose) => {
 
 /* PORTFOLIO SWIPER  */
 
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    // loop: true, //Allos Us To Loop Through Pagination
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    // mousewheel: true,
+    // keyboard: true,
+  });
 
 /* TESTIMONIAL */
 
