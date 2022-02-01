63a41ab3ce32b314a2b143c0d2840aada24ddba0
mucyoalexaxel <mucyoalexaxel@gmail.com>
2022-02-01 13:19:48 +0200

    Swiper JS Commented

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -113,20 +113,20 @@ modalCloses.forEach((modalClose) => {
 
 /* PORTFOLIO SWIPER  */
 
-let swiper = new Swiper(".portfolio__container", {
-    cssMode: true,
-    loop: false, //Allos Us To Loop Through Pagination
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    mousewheel: true,
-    keyboard: true,
-  });
+// let swiper = new Swiper(".portfolio__container", {
+//     cssMode: true,
+//     loop: false, //Allos Us To Loop Through Pagination
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     mousewheel: true,
+//     keyboard: true,
+//   });
 
 /* TESTIMONIAL */
 
