94343927478aef1097926ec9b9f3907ed32d5bad
mucyoalexaxel <mucyoalexaxel@gmail.com>
2022-02-01 14:17:01 +0200

    New Mongo Db Connection

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // Loading myResume Database In The Server
 
-if (process.env.NODE_ENV !== 'production'){
-    require('dotenv').config()
-}
+// if (process.env.NODE_ENV !== 'production'){
+//     require('dotenv').config()
+// }
 
 
 // Importing Libaries 
@@ -36,10 +36,19 @@ app.use('/', indexRouter)
 
 
 // Mongoose DataBase Connection
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log("Connected To Moongose Database Successfully"))
+// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+// const db = mongoose.connection
+// db.on('error', error => console.error(error))
+// db.once('open', () => console.log("Connected To Moongose Database Successfully"))
+
+// MongoDB Atlas Connection
+
+const uri = "mongodb+srv://Admin:CaineMvhzc@12@resume1.vdlq5.mongodb.net/Resume1?retryWrites=true&w=majority";
+
+const connectDB = async () => {
+    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    console.log('Connected To MongoDb Atlas Succefully')
+}
 
 // Connecting & Listening to The Database
 
