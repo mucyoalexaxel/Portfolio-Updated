e0abc1d1485b579c95672609dff7a3458c9a43b3
mucyoalexaxel <mucyoalexaxel@gmail.com>
2022-02-01 14:53:21 +0200

    1. Removed Mongoose Database 2. Added Mongo Atlas DataBase

--- server.js
+++ server.js
@@ -1,10 +1,3 @@
-// Loading myResume Database In The Server
-
-// if (process.env.NODE_ENV !== 'production'){
-//     require('dotenv').config()
-// }
-
-
 // Importing Libaries 
 
 const mongoose = require('mongoose')
@@ -27,19 +20,12 @@ app.set('views', __dirname + '/views')
 
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
-app.use(express.static('public')) // Why Does The JS Not Work?
+app.use(express.static('public'))
 
 
 // Using Routes
 
 app.use('/', indexRouter)
-
-
-// Mongoose DataBase Connection
-// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-// const db = mongoose.connection
-// db.on('error', error => console.error(error))
-// db.once('open', () => console.log("Connected To Moongose Database Successfully"))
 
 // MongoDB Atlas Connection
 
