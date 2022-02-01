51ecfc666ce5cc73adc3903f5fcf4d2747102590
mucyoalexaxel <mucyoalexaxel@gmail.com>
2022-02-01 13:29:22 +0200

    Uncomented SwiperJS (Comment .env in gitIgnore)

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -113,20 +113,20 @@ modalCloses.forEach((modalClose) => {
 
 /* PORTFOLIO SWIPER  */
 
-// let swiper = new Swiper(".portfolio__container", {
-//     cssMode: true,
-//     loop: false, //Allos Us To Loop Through Pagination
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-//     mousewheel: true,
-//     keyboard: true,
-//   });
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: false, //Allos Us To Loop Through Pagination
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
 
 /* TESTIMONIAL */
 
@@ -139,25 +139,25 @@ const formSubmit = (e) => {
     alert('Your Message Has Been Sent Successfully')
 }
 
-// /* SCROLL SECTIONS ACTIVE LINK */
-// const sections = document.querySelectorAll('section[id]')
+/* SCROLL SECTIONS ACTIVE LINK */
+const sections = document.querySelectorAll('section[id]')
 
-// function scrollActive(){
-//     const scrollY = window.pageYOffset
+function scrollActive(){
+    const scrollY = window.pageYOffset
 
-//     sections.forEach(current =>{
-//         const sectionHeight = current.offsetHeight
-//         const sectionTop = current.offsetTop - 50;
-//         sectionId = current.getAttribute('id')
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
 
-//         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-//         }else{
-//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-//         }
-//     })
-// }
-// window.addEventListener('scroll', scrollActive)
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /* CHANGE BACKGROUND HEADER */ 
 function scrollHeader(){
