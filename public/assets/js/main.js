b0bd39cba4bc89021c8ed87937e32ec85d08793e
mucyoalexaxel <mucyoalexaxel@gmail.com>
2022-02-01 10:50:03 +0200

    Uncomentend In Main Js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,8 +1,10 @@
+// import Swiper from 'https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js'
+
 /* MENU SHOW Y HIDDEN */
 
 const navMenu = document.getElementById('nav-menu'), 
     navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close');
+    navClose = document.getElementById('nav-close')
 
 
 /* SHOW MENU */
@@ -107,12 +109,13 @@ modalCloses.forEach((modalClose) => {
 //     })
 // })
 
+// Updated Swiper JS 
 
 /* PORTFOLIO SWIPER  */
 
 let swiper = new Swiper(".portfolio__container", {
     cssMode: true,
-    // loop: true, //Allos Us To Loop Through Pagination
+    loop: false, //Allos Us To Loop Through Pagination
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
@@ -121,8 +124,8 @@ let swiper = new Swiper(".portfolio__container", {
       el: ".swiper-pagination",
       clickable: true,
     },
-    // mousewheel: true,
-    // keyboard: true,
+    mousewheel: true,
+    keyboard: true,
   });
 
 /* TESTIMONIAL */
@@ -136,25 +139,25 @@ const formSubmit = (e) => {
     alert('Your Message Has Been Sent Successfully')
 }
 
-/* SCROLL SECTIONS ACTIVE LINK */
-const sections = document.querySelectorAll('section[id]')
+// /* SCROLL SECTIONS ACTIVE LINK */
+// const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+// function scrollActive(){
+//     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
+//     sections.forEach(current =>{
+//         const sectionHeight = current.offsetHeight
+//         const sectionTop = current.offsetTop - 50;
+//         sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
-}
-window.addEventListener('scroll', scrollActive)
+//         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+//         }else{
+//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+//         }
+//     })
+// }
+// window.addEventListener('scroll', scrollActive)
 
 /* CHANGE BACKGROUND HEADER */ 
 function scrollHeader(){
@@ -165,41 +168,41 @@ function scrollHeader(){
 window.addEventListener('scroll', scrollHeader)
 
 // /* SHOW SCROLL UP */ 
-// function scrollUp(){
-//     const scrollUp = document.getElementById('scroll-up')
-//     // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-//     if(this.scrollY >= 560) scrollUp.classList.add('show-scroll')
-//     else scrollUp.classList.remove('show-scroll')
-// }
-// window.addEventListener('scroll', scrollUp);
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll')
+    else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp);
 
 
 /* DARK LIGHT THEME */ 
-// const themeButton = document.getElementById('theme-button');
-// const darkTheme = 'dark-theme';
-// const iconTheme = 'uil-sun';
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
 
-// // Previously selected topic (if user selected)
-// const selectedTheme = localStorage.getItem('selected-theme');
-// const selectedIcon = localStorage.getItem('selected-icon');
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
 
-// // We obtain the current theme that the interface has by validating the dark-theme class
-// const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
-// const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
 
-// // We validate if the user previously chose a topic
-// if (selectedTheme) {
-//   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-//   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
-//   themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme);
-// }
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme);
+}
 
-// // Activate / deactivate the theme manually with the button
-// themeButton.addEventListener('click', () => {
-//     // Add or remove the dark / icon theme
-//     document.body.classList.toggle(darkTheme);
-//     themeButton.classList.toggle(iconTheme);
-//     // We save the theme and the current icon that the user chose
-//     localStorage.setItem('selected-theme', getCurrentTheme());
-//     localStorage.setItem('selected-icon', getCurrentIcon());
-// })
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+})
